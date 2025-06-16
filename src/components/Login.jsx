9912198abf1c1e05b6c8f7b5350212547333fa5d
Joao Morgado <jmrmorgado@gmail.com>
2025-06-16 09:21:39 -0400

    feat(signup): Signup now submits object with input values

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
+
 export function Login({ onLogin, onSignUp }) {
   const navigate = useNavigate();
   const [inputs, setInputs]= useState({})
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,24 +1,51 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
+
 export function SignUp({ onLogin }) {
-  function handleSubmit(event) {
-    event.preventDefault();
+  const navigate = useNavigate();
+  const [inputs, setInputs] = useState({});
+
+  function handleChange(evenet) {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs((values) => ({ ...values, [name]: value }));
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
 
-// TODO
-// Fetch POST /signup
-// {name:'', email:'', password_hash:''}
+    (async () => {
+      const response = await fetch('/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(inputs),
+      });
+
+      if (response.redirected) {
+        navigate('/market');
+      } else {
+        navigate('/signup');
+      }
+    })();
+  }
+
+  // TODO
+  // Fetch POST /signup
+  // {name:'', email:'', password_hash:''}
 
   return (
     <div className="loginSignUp">
       <img src="src/assets/GW_Logo.png" />
       <form onSubmit={handleSubmit}>
-        <label htmlFor="name">Name</label>
-        <input id="name" name="name" type="text"></input>
+        <label htmlFor="name">Username</label>
+        <input id="name" name="name" type="text" onChange={handleChange}></input>
         <label htmlFor="email">Email</label>
-        <input id="email" name="email" type="text"></input>
-        <label htmlFor="password">Password</label>
-        <input id="password" name="password" type="password"></input>
+        <input id="email" name="email" type="text" onChange={handleChange}></input>
+        <label htmlFor="password_hash">Password</label>
+        <input id="password_hash" name="password_hash" type="password" onChange={handleChange}></input>
         <Link to="/market">
           <input type="submit" value="Sign Up"></input>
         </Link>
